ed2e867d5405ac152723f98f94b3c30ce0a2b8d7
Stephan Sundermann <stephansundermann@gmail.com>
2014-04-10 14:37:49 +0200

    Initial commit

---
+++ sources/custom/Framebuffer.cs
@@ -0,0 +1,14 @@
+namespace Cogl {
+
+	using System;
+	using System.Collections;
+	using System.Collections.Generic;
+	using System.Runtime.InteropServices;
+
+	public partial class Framebuffer {
+
+		protected Framebuffer() : base (IntPtr.Zero)
+		{
+		}
+	}
+}
